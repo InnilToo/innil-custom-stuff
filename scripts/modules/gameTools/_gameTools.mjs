0c765287efc66f607f65d23ded2b1b9ef80fe159
Innil <boucekfilip@gmail.com>
2023-05-28 11:29:40 +0200

    Add `playerLanguages` and adjust `advanceTime`

--- scripts/modules/gameTools/_gameTools.mjs
+++ scripts/modules/gameTools/_gameTools.mjs
@@ -1,7 +1,9 @@
 import { advanceTime } from "./advanceTime.mjs";
 import { currentCombatant } from "./currentCombatant.mjs";
+import { playerLanguages } from "./playerLanguages.mjs";
 
 export const gameTools = {
   advanceTime,
   currentCombatant,
+  playerLanguages,
 };
