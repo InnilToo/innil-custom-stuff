830fd2c2220d32f7bbf2ca7f552b9186330bb060
Innil <boucekfilip@gmail.com>
2023-05-30 23:16:16 +0200

    Add animation to `spiritShroud`

--- scripts/modules/itemacros/spells/spiritShroud.mjs
+++ scripts/modules/itemacros/spells/spiritShroud.mjs
@@ -13,18 +13,36 @@ export async function SPIRIT_SHROUD(
   if (!ItemMacroHelpers._getDependencies(DEPEND.CN, DEPEND.VAE))
     return item.use();
 
-  const use = await item.use();
+  const use = await item.use(
+    { createMeasuredTemplate: false },
+    { configureDialog: false }
+  );
   if (!use) return;
 
+  const conc = await CN.waitForConcentrationStart(actor, { item });
+  if (!conc) return;
+
   const buttons = [
-    { icon: "snowflake", type: "cold" },
-    { icon: "skull", type: "necrotic" },
-    { icon: "holly-berry", type: "radiant" },
-  ].reduce((acc, { icon, type }) => {
+    {
+      icon: "snowflake",
+      type: "cold",
+      file: "jb2a.spirit_guardians.blue.ring",
+    },
+    {
+      icon: "skull",
+      type: "necrotic",
+      file: "jb2a.spirit_guardians.dark_black.ring",
+    },
+    {
+      icon: "holly-berry",
+      type: "radiant",
+      file: "jb2a.spirit_guardians.dark_whiteblue.ring",
+    },
+  ].reduce((acc, { icon, type, file }) => {
     acc[type] = {
       icon: `<i class="fa-solid fa-${icon}"></i>`,
       label: type.capitalize(),
-      callback: flagEffect,
+      callback: (html, event) => flagEffect(html, event, file),
     };
     return acc;
   }, {});
@@ -35,7 +53,7 @@ export async function SPIRIT_SHROUD(
     buttons,
   }).render(true);
 
-  async function flagEffect(html, event) {
+  async function flagEffect(html, event, file) {
     const type = event.currentTarget.dataset.button;
     const effect = CN.isActorConcentratingOnItem(actor, item);
     const level = effect.flags[DEPEND.CN].data.castData.castLevel;
@@ -45,6 +63,17 @@ export async function SPIRIT_SHROUD(
       { key: "system.bonuses.mwak.damage", mode, value },
       { key: "system.bonuses.msak.damage", mode, value },
     ];
+
+    await new Sequence()
+      .effect()
+      .attachTo(token)
+      .tieToDocuments(conc)
+      .file(file)
+      .scale(0.66667)
+      .fadeIn(500)
+      .persist()
+      .play({ remote: true });
+
     return effect.update({ changes });
   }
 }
